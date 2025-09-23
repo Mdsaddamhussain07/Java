85b5684af6624f72d7153004d03d68b3618c81cc
Md Saddam Hussain <mdsaddamhussain202005@gmail.com>
2025-09-23 23:45:27 +0530

    orderagnostic

--- 2 Binary Search/BinarySearch.java
+++ 2 Binary Search/BinarySearch.java
@@ -29,3 +29,37 @@ public class BinarySearch {
     }
     
 }
+
+
+// public class BinarySearch {
+//     public static void main(String[] args){
+//         int arr[] ={-12,-8,-4,0,2,3,4,15,16,18,22,45,89};
+//         int target = 22;
+//         int result = binarySearch(arr,target);
+//         System.out.println("Target " +target+ " found at index " +result);
+//     }
+//     static int binarySearch(int[] arr,int target){
+//         int start = 0;
+//         int end = arr.length -1;
+
+//         while(start<=end){
+//             // find the middle element
+//             int mid= start +(end - start)/2;
+
+//             if(target==arr[mid]){
+//                 return mid;
+//             }
+
+//             if(target<arr[mid]){
+//                 end = mid -1;
+//             }
+//             else {
+//                 start = mid +1;
+
+//             }
+//             
+//         }
+//         return -1;
+//     }
+    
+// }
--- Basic Java/Void.java
+++ Basic Java/Void.java
@@ -1,5 +1,5 @@
 public class Void {
-    static void search1(int[] arr,int target){
+    static void search(int[] arr,int target){
     for(int i=0;i<arr.length;i++){
         if(arr[i]==target){
             System.out.println("Element found at index "+i);
@@ -13,7 +13,7 @@ public static void main(String[] args) {
     int[] numbers ={6,4,3};
     // int target = 3;
     
-    search1(numbers,3);
+    search(numbers,3);
     
 
 }
